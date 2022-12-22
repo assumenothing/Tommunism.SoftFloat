fc40113eb5341dfc0e09e2f0e39e2be5158795f5
Thomas Kaiser <5789131+assumenothing@users.noreply.github.com>
2022-12-21 22:35:54 -0800

    Removed unused ApproxRecip_1k0s and ApproxRecip_1k1s internal arrays.
    These aren't needed if using the "fast 64-bit integer divide" implementation of ApproxRecip32_1.

--- Tommunism.SoftFloat/Primitives.cs
+++ Tommunism.SoftFloat/Primitives.cs
@@ -118,20 +118,6 @@ internal static class Primitives
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int CountLeadingZeroes64(ulong a) => BitOperations.LeadingZeroCount(a);
 
-    // softfloat_approxRecip_1k0s
-    internal static readonly ushort[] ApproxRecip_1k0s = new ushort[16]
-    {
-        0xFFC4, 0xF0BE, 0xE363, 0xD76F, 0xCCAD, 0xC2F0, 0xBA16, 0xB201,
-        0xAA97, 0xA3C6, 0x9D7A, 0x97A6, 0x923C, 0x8D32, 0x887E, 0x8417
-    };
-
-    // softfloat_approxRecip_1k1s
-    internal static readonly ushort[] ApproxRecip_1k1s = new ushort[16]
-    {
-        0xF0F1, 0xD62C, 0xBFA1, 0xAC77, 0x9C0A, 0x8DDB, 0x8185, 0x76BA,
-        0x6D3B, 0x64D4, 0x5D5C, 0x56B1, 0x50B6, 0x4B55, 0x4679, 0x4211
-    };
-
     // softfloat_approxRecip32_1 -- assumes fast 64-bit integer divide is available
     /// <summary>
     /// Returns an approximation to the reciprocal of the number represented by <paramref name="a"/>, where <paramref name="a"/> is
