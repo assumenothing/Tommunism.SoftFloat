e1b79b09a772ebee392d36da7650a8d337adebb5
Thomas Kaiser <5789131+assumenothing@users.noreply.github.com>
2022-12-27 17:58:01 -0800

    Added casing support to ValueStringBuilder.AppendHex.

--- src/Tommunism.SoftFloat/ValueStringBuilder.AppendHex.cs
+++ src/Tommunism.SoftFloat/ValueStringBuilder.AppendHex.cs
@@ -4,9 +4,10 @@ namespace System.Text;
 
 partial struct ValueStringBuilder
 {
-    private static ReadOnlySpan<byte> HexChars => "0123456789ABCDEF"u8;
+    private static ReadOnlySpan<byte> HexCharsUpper => "0123456789ABCDEF"u8;
+    private static ReadOnlySpan<byte> HexCharsLower => "0123456789abcdef"u8;
 
-    public void AppendHex(uint value, int bitCount)
+    public void AppendHex(uint value, int bitCount, bool isLowerCase = false)
     {
         Debug.Assert(bitCount is > 0 and <= int.MaxValue - 3);
         var hexCharCount = (bitCount + 3) / 4;
@@ -17,15 +18,16 @@ partial struct ValueStringBuilder
 
         // Reserve characters in builder for encoded hex data.
         var buffer = AppendSpan(hexCharCount);
+        var hexAlphabet = isLowerCase ? HexCharsLower : HexCharsUpper;
         for (int shift = (hexCharCount - 1) * 4, i = 0; shift >= 0; shift -= 4, i++)
         {
             // Out of range shifts are always treated as zero values.
             var c = (shift < 32) ? ((int)(value >> shift) & 0xF) : 0;
-            buffer[i] = (char)HexChars[c];
+            buffer[i] = (char)hexAlphabet[c];
         }
     }
 
-    public void AppendHex(ulong value, int bitCount)
+    public void AppendHex(ulong value, int bitCount, bool isLowerCase = false)
     {
         Debug.Assert(bitCount is > 0 and <= int.MaxValue - 3);
         var hexCharCount = (bitCount + 3) / 4;
@@ -36,11 +38,12 @@ partial struct ValueStringBuilder
 
         // Reserve characters in builder for encoded hex data.
         var buffer = AppendSpan(hexCharCount);
+        var hexAlphabet = isLowerCase ? HexCharsLower : HexCharsUpper;
         for (int shift = (hexCharCount - 1) * 4, i = 0; shift >= 0; shift -= 4, i++)
         {
             // Out of range shifts are always treated as zero values.
             var c = (shift < 64) ? ((int)(value >> shift) & 0xF) : 0;
-            buffer[i] = (char)HexChars[c];
+            buffer[i] = (char)hexAlphabet[c];
         }
     }
 }
